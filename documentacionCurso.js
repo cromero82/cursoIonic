c6f0e6807820f2918be741381c0507f8e9d8e5fb
carlos romero <soportecarlosromero@gmail.com>
2019-11-17 07:40:38 -0500

    Creación de una pagina nueva con menu

---
+++ documentacionCurso.js
@@ -0,0 +1,10 @@
+// ------------------ agregar una nueva pagina
+// 1. ionic g page listadoNoticias
+// 2. agregar ruta
+// 3. agregar en pagina: <...> para colocar el iconomenu en la seccion inicial de la nueva pagina
+<ion-toolbar>
+<ion-buttons slot="start">
+  <ion-menu-button></ion-menu-button>
+</ion-buttons>
+<ion-title>listadoNoticias</ion-title>
+</ion-toolbar>
